3b15231afd4823a6f87ae6acd639263203d657c7
Rifat Rabbi <rifayrabby78@gmail.com>
2023-11-05 12:30:09 +0600

    filter available room data by id from database completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,14 @@ async function run() {
         .toArray();
       res.send(result);
     });
+    // filter data by id to show details page
+    app.get("/room/details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
